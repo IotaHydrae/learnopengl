663d38c678576c2570c46ad986927430eb5bb6d1
hz <writeforever@foxmail.com>
2021-12-08 00:41:12 -0800

    finished triangle create.

--- demo_opengl_create_triangle/main.c
+++ demo_opengl_create_triangle/main.c
@@ -22,7 +22,6 @@ void processInput(GLFWwindow *window)
 
 int main(int argc, char **argv)
 {
-	printf("Hello, World!\n");
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -47,6 +46,96 @@ int main(int argc, char **argv)
 	glViewport(0,0,800,600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	unsigned int VAO;
+	unsigned int VBO;
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f, 
+	};
+
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	/* generate a vertex buffer object with a ID */
+	glGenBuffers(1, &VBO);
+
+	/* bind this VBO type to `GL_ARRAY_BUFFER` */
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	/* copy the previously defined vertex data into the buffer's memory */
+	/* `GL_STATIC_DRAW` because the data filled in that nerver changed */
+	glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	/* set the vertex attributes pointers */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	/* basic vertex shader in GLSL(could read from file) */
+	const char *vertexShaderSource = "#version 330 core\n"
+	    "layout (location = 0) in vec3 aPos;\n"
+	    "void main()\n"
+	    "{\n"
+	    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	    "}\0";	
+
+	/* compiling a shader */
+	/* create a shader */
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	/* attach the shader source code to the shader object and compile the shader. */
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	/* TODO:error check of shader compiling here. */
+	int succss;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &succss);
+	if(!succss)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("ERROR: SHADER vertex compile failed!\n");
+	}
+
+	/**
+	 * `fragment shader` the second step to render a triangle.
+	 * the fragment shader is all about calculating the color
+	 * output of your pixels.
+	 */
+	/* do the same thing with vertexShaderSource */
+	const char *fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n"
+		"}\0";
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &succss);
+	if(!succss)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("ERROR: SHADER fragmentShader compile failed!\n");
+	}
+
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+
+
 	/* render loop */
 	while(!glfwWindowShouldClose(window))
 	{
@@ -56,6 +145,11 @@ int main(int argc, char **argv)
 		/* rendering commands here */
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* check and call events and swap the buffers */
 		glfwSwapBuffers(window);
